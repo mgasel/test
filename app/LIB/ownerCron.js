6a4eb4f7a64ba66b66818f830bccfd840aea6e0c
kartik <kartik@apptunix.com>
2020-06-23 10:12:33 +0530

    changes in cron job

--- app/LIB/ownerCron.js
+++ app/LIB/ownerCron.js
@@ -4,17 +4,17 @@ exports.assignDriver = async()=>{
     cron.schedule(' * * * * *', async ()=> {
 
     //   console.log('hello how are you');
-      const findDriver = await bookingModel.find({driverId:null})
+      // const findDriver = await bookingModel.find({driverId:null})
     //   console.log('findDriver',findDriver);
-    let nearDriver = await userModel.findOne({$and:[{    currentLocation:
-        { $near :
-           {
-             $geometry: { type: "Point",  coordinates: [30.712905, 
-                76.709302 ] },
-             $minDistance: 0,
-             $maxDistance: 10000
-           }
-        }},{ userType : "DRIVER"},{isOnline : true},{isAvailable: true}]})
+    // let nearDriver = await userModel.findOne({$and:[{    currentLocation:
+    //     { $near :
+    //        {
+    //          $geometry: { type: "Point",  coordinates: [30.712905, 
+    //             76.709302 ] },
+    //          $minDistance: 0,
+    //          $maxDistance: 10000
+    //        }
+    //     }},{ userType : "DRIVER"},{isOnline : true},{isAvailable: true}]})
       
       
 
