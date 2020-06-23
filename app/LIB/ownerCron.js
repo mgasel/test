623b4c5ffd1a92ba4e74d06ac0ef11071af9bdfd
kartik <kartik@apptunix.com>
2020-06-23 11:21:04 +0530

    assign the driver

--- app/LIB/ownerCron.js
+++ app/LIB/ownerCron.js
@@ -1,23 +1,32 @@
 const cron = require('node-cron')
 const bookingModel = require('../models/Bookings')
+const userModel = require('../models/User')
 exports.assignDriver = async()=>{
-    cron.schedule(' * * * * *', async ()=> {
+    cron.schedule('*/10 * * * * *', async ()=> {
 
-    //   console.log('hello how are you');
-      // const findDriver = await bookingModel.find({driverId:null})
-    //   console.log('findDriver',findDriver);
-    // let nearDriver = await userModel.findOne({$and:[{    currentLocation:
-    //     { $near :
-    //        {
-    //          $geometry: { type: "Point",  coordinates: [30.712905, 
-    //             76.709302 ] },
-    //          $minDistance: 0,
-    //          $maxDistance: 10000
-    //        }
-    //     }},{ userType : "DRIVER"},{isOnline : true},{isAvailable: true}]})
+      // console.log('hello how are you');
+      const findDriver = await bookingModel.find({driverId:null})
+      // console.log('findDriver',findDriver);
+    await  findDriver.map(async(bookings,index)=>{
+        
+          let nearDriver = await userModel.findOne({$and:[{    currentLocation:
+        { $near :
+           {
+             $geometry: { type: "Point",  coordinates: [bookings.pickUpLat, 
+              bookings.pickUpLong] },
+             $minDistance: 0,
+             $maxDistance: 10000
+           }
+        }},{ userType : "DRIVER"},{isOnline : true},{isAvailable: true}]})
       
+        if(nearDriver!=null){
+          await bookingModel.update({_id:bookings._id},{driverId:nearDriver._id})
+          await userModel.update({_id:nearDriver._id},{isAvailable: false})
+        }
+        
+      })
+ 
       
-
 
 
     })
