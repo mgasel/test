d7df8e10701d537fff7d33b63705880b69753fc0
kartik <kartik@apptunix.com>
2020-06-26 14:50:10 +0530

    add auth in booking api

--- app/routes/owneroutes.js
+++ app/routes/owneroutes.js
@@ -29,7 +29,7 @@ module.exports= function(app){
     app.put('/owner/updatePrice',authenticattion.verifyOwnerBranch,ownerController.updaetPrice)
     // app.post('/owner/getServices')
     app.post('/owner/service',authenticattion.verifyOwnerBranch,ownerController.getLists)
-    app.post('/owner/createBookings',ownerController.createBooking)
+    app.post('/owner/createBookings',authenticattion.verifyOwnerBranch,ownerController.createBooking)
     app.post('/owner/laundryItemPrice',authenticattion.verifyOwnerBranch,ownerController.itemPrice) // listing item price
     app.post('/owner/laundryData',authenticattion.verifyOwnerBranch,ownerController.laundryDetails)
     app.post('/owner/laundryDetails',authenticattion.verifyOwnerBranch,ownerController.laundryServices)
